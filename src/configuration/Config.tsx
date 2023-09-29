6e03a2802cad8c89f7872a7f556c9eb3864f9ef0
Heroes Battle Arena <106152338+heroesbattlearena@users.noreply.github.com>
2023-09-29 16:32:06 +0300

    Update Config.tsx

--- src/configuration/Config.tsx
+++ src/configuration/Config.tsx
@@ -21,13 +21,13 @@ export const NETWORK_ERROR = "Unsuppoted Blockchain Network or Domain Name !" //
 export const DOMAIN_TLD = 'yak' //primary domain name without dot (.)
 export const DOMAIN_PRICE_ETH = '0.1' //price should be equal to contract or higher 
 export const DOMAIN_IMAGE_URL = 'https://w3d.name/api/nft/yak.jpg' //Image path starts with ipfs:// or https://
-export const DOMAIN_NETWORK_CHAIN = 314 //137 for polygon, 314 for filecoin, 80001 form mumbai
+export const DOMAIN_NETWORK_CHAIN = 137 //137 for polygon, 314 for filecoin, 80001 form mumbai
 export const DOMAIN_DESCRIPTION = 'My Domain description goes here....'
 export const DOMAIN_TYPE = "W3D" //W3D for polygon, FVM for Filecoin net
 export const DOMAIN_TITLE = "Web3 Domain Search" //Title above the search input field. 
 export const DOMAIN_PLACEHOLDER = "Search for a name" //Placeholder for search input field 
 
-export const ADMIN_WALLET = "0x8D714B10B719c65B878F2Ed1436A964E11fA3271" //ETH wallet address 
+export const ADMIN_WALLET = "0x2f325Af79B77518186862dA3775C9E432301C82a" //ETH wallet address 
 
 export const NOTICE_TITLE = "Bulletin board"
 export const NOTICE_NON_MEMBER = "Only the .yak domain holder can view bulletin board."
